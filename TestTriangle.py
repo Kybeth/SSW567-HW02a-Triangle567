e7d86dff9ea1b1292b954adf32ec842507e9ddff
Yuan <byrd.zhang@gmail.com>
2019-03-13 02:10:27 -0400

    commit for part1

--- TestTriangle.py
+++ TestTriangle.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 """
+@author: Yuan Zhang
+SSW567 HW02a
+---
+Original info:
+
 Updated Jan 21, 2018
 The primary goal of this file is to demonstrate a simple unittest implementation
 
@@ -14,19 +19,35 @@ from Triangle import classifyTriangle
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
 
+
 class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
-    def testRightTriangleA(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
+    def testRightTriangleA(self):
+        self.assertEqual(classifyTriangle(3, 4, 5), 'Right',
+                         '3,4,5 is a Right triangle')
 
-    def testRightTriangleB(self): 
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
-        
-    def testEquilateralTriangles(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+    def testRightTriangleB(self):
+        self.assertEqual(classifyTriangle(5, 3, 4), 'Right',
+                         '5,3,4 is a Right triangle')
+
+    def testIsoscelesTriangle(self):
+        self.assertEqual(classifyTriangle(5, 4, 4), 'Isosceles',
+                         '5,4,4 is a Isosceles triangle')
+
+    def testScaleneTriangle(self):
+        self.assertEqual(classifyTriangle(5, 4, 6), 'Scalene',
+                         '5,4,6 is a Scalene triangle')
+
+    def testEquilateralTriangles(self):
+        self.assertEqual(classifyTriangle(1, 1, 1),
+                         'Equilateral', '1,1,1 should be equilateral')
+
+    def testNotTriangle(self):
+        self.assertEqual(classifyTriangle(2, 1, 5), 'NotATriangle',
+                         '2,1,5 is Not A Triangle')
+
 
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main()
-
---
+++ testReport1.md
@@ -0,0 +1,11 @@
+# Test report for initial classifyTriangle()
+| Test ID | Input | Expected Results | Actual Result | Pass or Fail
+| --- | --- | --- | --- | --- |
+| 1 | 3,4,5 | Right | InvalidInput | Fail |
+| 2 | 5,3,4 | Right | InvalidInput | Fail |
+| 3 | 5,4,4 | Isosceles | InvalidInput | Fail |
+| 4 | 5,4,6 | Scalene | InvalidInput | Fail |
+| 5 | 1,1,1 | Equilateral | InvalidInput | Fail |
+| 6 | 2,1,5 | NotATriangle | InvalidInput | Fail |
+
+
